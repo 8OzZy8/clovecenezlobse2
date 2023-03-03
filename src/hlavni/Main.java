0d61799290644f439fd7a45f3d2e20c721a420ea
8OzZy8 <jstepanekjan@gmail.com>
2023-03-03 12:48:38 +0100

    0.01

--- src/hlavni/Main.java
+++ src/hlavni/Main.java
@@ -1,7 +1,17 @@
 package hlavni;
+import javax.swing.*;
+import java.util.*;
+import javax.swing.JFrame;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        program p = new program();
+        JFrame okno = new JFrame("Člověče nezlob se!");
+        okno.setContentPane(p.panel1);
+        okno.setMinimumSize(new Dimension(500,500));
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.pack();
+        okno.setVisible(true);
     }
 }
---
+++ src/hlavni/kostka.java
@@ -0,0 +1,11 @@
+package hlavni;
+import java.util.*;
+public class kostka {
+    static public int randomkostka(){
+        Random r=new Random();
+        int cislo = r.nextInt(6);
+
+
+        return(cislo);
+    }
+}
